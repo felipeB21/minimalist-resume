ebd872921b8083e6e964332093493202a4e2b8ba
felipeB21 <bolgarfelipe@gmail.com>
2024-02-01 15:46:00 -0300

    Added Skills and Finish project.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,14 +2,16 @@ import About from "@/components/About";
 import Education from "@/components/Education";
 import Hero from "@/components/Hero";
 import Proyects from "@/components/Proyects";
+import Skills from "@/components/Skills";
 
 export default function Home() {
   return (
-    <main className="w-[680px] mx-auto mt-14">
+    <main className="w-[680px] mx-auto mt-14 mb-14">
       <Hero />
       <About />
       <Education />
       <Proyects />
+      <Skills />
     </main>
   );
 }
